bc8b4ef8020e908d85253403012f75a4e4d24531
KakasInfinity <159872426+KakasInfinity@users.noreply.github.com>
2024-02-18 18:30:49 -0600

    Add files via upload
    Primera pagina de omega up

---
+++ PARCIAL 1/tarea 1.3/Tarea 2.0/12662C16.php
@@ -0,0 +1,18 @@
+<html>
+    <head></head>
+    <body>
+        <h1 >12662. C16</h1>
+  <table border="1">
+    <tr>
+        <td><h4>Puntos</h4></td>
+        <td>16.79</td>
+        <td><h4>Limite de <br> memoria</h4></td>
+        <td>32 Mib</td>
+    </tr>
+  </table>      
+    
+<?php
+
+?>
+</body>
+</html>
